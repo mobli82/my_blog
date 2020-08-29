c8f88eb25ed3eacbf714658338beade11dd82030
mobli <moblixxyy@gmail.com>
2020-08-29 12:22:44 +0200

    added register path in app_blog/settings.py
    created registe.html in users/templates
    
    creted UserRegistrationForm in users/forms.py
    
    addef path fro register view in users/views.py
    
    created register function in users/views.py

--- app_blog/users/views.py
+++ app_blog/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistartionForm
 
 # Create your views here.
+
+def register(request):
+
+    if request.method == 'POST':
+        form = UserRegistartionForm(request.POST)
+
+        if form.is_valid():
+            messages.success(request, f'Your Account has been created successfully')
+            form.save()
+    
+    else:
+        form = UserRegistartionForm()
+
+
+    return render(request, 'users/register.html', {'form': form})
