1bf115dce8bff67aa4bed75d8d7ec736b18de500
mobli <moblixxyy@gmail.com>
2020-08-29 13:42:34 +0200

    created logout.html template added path in app_blog/settings.py
    created footer.html file and inluded in base.html

--- app_blog/app_blog/urls.py
+++ app_blog/app_blog/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', include('users.urls')),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
