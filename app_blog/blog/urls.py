1f5cbe268c98d4ed1686b11b94aa14b5e56ea0cc
mobli <moblixxyy@gmail.com>
2020-08-29 15:35:24 +0200

    created PostCreatedView in blog/views.py
    added path in blog/urls.py for PostCeateView in blog/views.py
    
    added link for create post in nav_bar.html

--- app_blog/blog/urls.py
+++ app_blog/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostCreateView
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='posts'),
+    path('post-create/', PostCreateView.as_view(), name='post-create'),
 ]
--- app_blog/blog/views.py
+++ app_blog/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (ListView,
+                                  CreateView,
+)
 
 from .models import Post
 
@@ -8,3 +12,14 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+class PostCreateView(LoginRequiredMixin, SuccessMessageMixin,CreateView):
+    model = Post
+    template_name = 'blog/create_post.html'
+    fields = ['title', 'content']
+    success_url = '/blog/posts'
+    success_message = 'Post has been created'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
