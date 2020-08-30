d518bdfeb49256a69c1aaa8e40db4a6467dfca99
mobli <moblixxyy@gmail.com>
2020-08-30 09:32:08 +0200

    renamed file home.html for posts_blog.html renamed file create_post.html for post_create.html
    created post_detail.html template
    created PostDeatailView
    
    added path in app_blog/urls.py fro PostDetailView

--- app_blog/blog/urls.py
+++ app_blog/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListView, PostCreateView
+from .views import PostListView, PostCreateView, PostDetailView
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='posts'),
     path('post-create/', PostCreateView.as_view(), name='post-create'),
+    path('post-detail/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
--- app_blog/blog/views.py
+++ app_blog/blog/views.py
@@ -1,25 +1,37 @@
 from django.shortcuts import render
-from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (ListView,
                                   CreateView,
+                                  DetailView,
 )
 
 from .models import Post
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'
+    template_name = 'blog/posts_blog.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
-class PostCreateView(LoginRequiredMixin, SuccessMessageMixin,CreateView):
+class PostCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Post
-    template_name = 'blog/create_post.html'
+    template_name = 'blog/post_create.html'
     fields = ['title', 'content']
     success_url = '/blog/posts'
-    success_message = 'Post has been created'
+    success_message = '%(title)s has been created'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def get_success_message(self, title):
+        return self.success_message % dict(
+            title,
+            calculated_field=self.object.title,
+        )
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
