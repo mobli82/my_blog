a13dfa075dd79ae83cbfed3a71f9d6a865c277f4
mobli <moblixxyy@gmail.com>
2020-08-30 10:37:53 +0200

    added PosatUpdateView in blog/views.py

--- app_blog/blog/views.py
+++ app_blog/blog/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (ListView,
                                   CreateView,
                                   DetailView,
+                                  UpdateView,
 )
 
 from .models import Post
@@ -35,3 +36,16 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
