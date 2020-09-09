639dec0e5f3d6ad2098713c9db40798d3aabcf44
mobli <moblixxyy@gmail.com>
2020-09-09 11:42:09 +0200

    testing users/signals.py tests don't pass FIX THAT !!!

--- app_blog/users/tests/test_userss_signal_create_profile.py
+++ app_blog/users/tests/test_userss_signal_create_profile.py
@@ -1,18 +1,26 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.db.models import signals
 
 from users.models import Profile
 import mock
 
 class TestSaveProfile(TestCase):
 
-    def test_should_send_signal_to_create_profile(self):
-        user = User.objects.create_user('New user', 'new_user@company.com')
-        user.save()
+    @mock.patch('users.signals.create_profile')
+    def test_should_send_signal_to_create_profile(self, create_profile):
+        signals.post_save.disconnect(sender=User)
 
-        from django.db.models import signals
-        from users.signals import create_profile
+        create_profile.created = True
+                
+        if create_profile.created:
+            Profile.objects.create(user=User.objects.create_user('Some user'))
 
-        reg_func = [func[1]() for func in signals.post_save.receivers]
-        self.assertIn(create_profile, reg_func)
+        self.assertTrue(create_profile.called)
+
+        # from django.db.models import signals
+        # from users.signals import create_profile
+
+        # reg_func = [func[1]() for func in signals.post_save.receivers]
+        # self.assertIn(create_profile, reg_func)
         
